874297bf599afa096b7941c58e7a2dc347764fdd
churchillexe@gmail.com <tingexe@DESKTOP-SQCUPQ8.>
2024-10-21 21:24:31 +0800

    update config files for nvim integration

---
+++ .config/nvim/lua/tingexe/plugins/integration.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'marilari88/neotest-vitest',
+    },
+    config = function()
+      require('neotest').setup {
+        ...,
+        adapters = {
+          require 'neotest-vitest' {
+            -- Filter directories when searching for test files. Useful in large projects (see Filter directories notes).
+            filter_dir = function(name, rel_path, root)
+              return name ~= 'node_modules'
+            end,
+          },
+        },
+      }
+    end,
+  },
+}
